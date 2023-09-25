c0250aff63ef17863efd340d4971a1d7bf3f28dc
v-alex-dev <vensalex1991@gmail.com>
2023-09-25 16:54:28 +0200

    list display

---
+++ assets/js/components/card/card-list.js
@@ -0,0 +1,50 @@
+const cardList = (item) => {
+	const section = document.getElementById("card");
+	const card = document.createElement('article');
+	const img = document.createElement('img');
+	const titleCard = document.createElement('h3');
+	const price = document.createElement('p');
+	const btnShop = document.createElement('button')
+	
+	img.className = 'dishes-img'
+	titleCard.className = 'title-card';
+	price.className = 'price';
+	btnShop.className = 'btn-shop';
+	btnShop.innerHTML = 'add'
+	
+	console.log(item);
+
+	
+	//create header to card with card-header className
+	const cardHeader = document.createElement('div');
+	cardHeader.className = 'card-header';
+
+	//create body to card with card-body className
+	const cardBody = document.createElement('div');
+	cardBody.className = 'card-body';
+
+	//create footer to card with card-footer className
+	const cardFooter = document.createElement('div');
+	cardFooter.className = 'card-footer';
+
+	section.appendChild(card);
+	card.appendChild(cardHeader);
+	card.appendChild(cardBody);
+	card.appendChild(cardFooter);
+
+	cardHeader.appendChild(img);
+	cardBody.appendChild(titleCard);
+	cardBody.appendChild(price);
+	cardFooter.appendChild(btnShop);
+
+	img.src = item.url;
+	titleCard.innerHTML = item.name;
+	price.innerHTML = item.price;
+
+
+
+}
+
+
+
+export {cardList};
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
+import { cardList } from "./components/card/card-list.js";
 import { DishesList } from "./data/Dishes-list.js";
+
+
+
+DishesList.forEach(element => {
+	cardList(element);
+});
