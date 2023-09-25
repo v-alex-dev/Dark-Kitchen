17cb35b238105153ca21ded092eeeb6b2d6b208a
v-alex-dev <vensalex1991@gmail.com>
2023-09-25 13:56:57 +0200

    save

--- assets/js/data/Dishes-list.js
+++ assets/js/data/Dishes-list.js
@@ -1,64 +1,155 @@
 const DishesList = [
 	{
+		id:1,
 		name: 'Sushi Salmon Teriyaki',
 		category: ['Sushi', 'Teryaki'],
 		ingredient: ['Salmon'],
-		url: '',
+		url: '../img/Dishes/su-sal-teriya.png',
 		price : 4.80
 	},
 	{
+		id:2,
 		name: 'Sushi Salmon',
 		category: ['Sushi'],
 		ingredient : ['Salmon'],
+		url: '../img/Dishes/su-sal-teriya.png',
 		price : 3.80
 	},
 	{
+		id:3,
 		name: 'Sushi Avocado ',
 		category: ['Sushi'],
 		ingredient : ['Avocado'],
+		url: '../img/Dishes/su-advo.png',
 		price : 3.30
 	},
 	{
+		id:4,
 		name: 'Sushi Shrimps ',
 		category: ['Sushi'],
 		ingredient : ['Shrimps'],
+		url: '../img/Dishes/su-shrimps.png',
 		price : 3.50
 	},
 	{
+		id:5,
 		name: 'Sushi Omelettes ',
 		category: ['Sushi'],
 		ingredient : ['Egg'],
+		url: '../img/Dishes/su-omel.png',
 		price : 3.20
 	},
 	{
+		id:6,
 		name: 'Sushi Tuna ',
 		category: ['Sushi'],
 		ingredient : ['Tuna'],
+		url: '../img/Dishes/su-tun.png',
 		price : 3.90
 	},
 	{
+		id:7,
 		name: 'Sushi Tuna Teriyaki ',
-		category: ['Sushi', 'Teriyako'],
+		category: ['Sushi', 'Teriyaki'],
 		ingredient : ['Tuna'],
+		url: '../img/Dishes/su-tun-teriya.png',
 		price : 4.20
 	},
 	{
+		id:8,
 		name: 'Gunkan Salmon egg',
 		category: ['Gunkan', 'Sushi'],
 		ingredient : ['Egg', 'Salmon'],
-		price : 4.20
+		url: '../img/Dishes/gun-egg-sal.png',
+		price : 5.00
 	},
 	{
+		id:9,
 		name: 'Gunkan masago',
 		category: ['Gunkan', 'Sushi'],
 		ingredient : ['Egg'],
-		price : 4.20
+		url: '../img/Dishes/gun-egg.png',
+		price : 5.10
 	},
 	{
+		id:10,
 		name: 'Gunkan Salmon tartar',
 		category: ['Gunkan', 'Sushi'],
 		ingredient : ['Salmon'],
-		price : 4.30
+		url: '../img/Dishes/gun-tart-sal.png',
+		price : 5.30
+	},
+	{
+		id:11,
+		name: 'California Salmon Advocado',
+		category: ['Maki', 'California'],
+		ingredient : ['Advocado', 'Salmon'],
+		url: '../img/Dishes/cal-adv-sal.png',
+		price : 5.50
+	},
+	{
+		id:12,
+		name: 'California Tuna Advocado',
+		category: ['Maki', 'California'],
+		ingredient : ['Advocado', 'Tuna'],
+		url: '../img/Dishes/cal-adv-tuna.png',
+		price : 5.60
+	},
+	{
+		id:13,
+		name: 'California Surimi Advocado',
+		category: ['Maki', 'California'],
+		ingredient : ['Advocado', 'Surimi'],
+		url: '../img/Dishes/cal-adv-suri.png',
+		price : 5.40
+	},
+	{
+		id:14,
+		name: 'California Chicken Tempura',
+		category: ['Maki', 'California'],
+		ingredient : ['Salad', 'Chicken'],
+		url: '../img/Dishes/cal-chicken.png',
+		price : 5.90
+	},
+	{
+		id:15,
+		name: 'Spring Rolls Cheese Advocado',
+		category: ['Maki', 'Spring Rolls'],
+		ingredient : ['Advocado', 'Cheese'],
+		url: '../img/Dishes/spring-rolls-adv.png',
+		price : 6.10
+	},
+	{
+		id:16,
+		name: 'Spring Rolls Salmon Advocado',
+		category: ['Maki', 'Springs Rolls'],
+		ingredient : ['Advocado', 'Salmon', 'Salad'],
+		url: '../img/Dishes/spring-rolls-adv-sal.png',
+		price : 6.20
+	},
+	{
+		id:17,
+		name: 'Spring Rolls Tuna Advocado',
+		category: ['Maki', 'California'],
+		ingredient : ['Advocado', 'Tuna', 'Salad'],
+		url: '../img/Dishes/spring-rolls-adv-tuna.png',
+		price : 6.40
+	},
+	{
+		id:18,
+		name: 'Temaki shrimps advocado',
+		category: ['Temaki'],
+		ingredient : ['Advocado', 'Tuna', 'Salad'],
+		url: '../img/Dishes/spring-rolls-adv-tuna.png',
+		price : 7.10
+	},
+	{
+		id:19,
+		name: 'Temaki shrimps advocado',
+		category: ['Temaki'],
+		ingredient : ['Advocado', 'Tuna', 'Salad'],
+		url: '../img/Dishes/spring-rolls-adv-tuna.png',
+		price : 7.10
 	},
 ]
 
