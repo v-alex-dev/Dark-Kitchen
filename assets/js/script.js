34232b31d2c46992a28feb5e34684ea7c4aab5c2
Barbier <mathbarbier07@gmail.com>
2023-09-26 12:21:17 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 import { cardList } from "./components/card/card-list.js";
 import { DishesList } from "./data/Dishes-list.js";
-import { burgerMenu } from "./components/burger-menu.js";
+import { burgerMenu } from "./function/burger-menu.js";
 
 DishesList.forEach((element) => {
   cardList(element);
