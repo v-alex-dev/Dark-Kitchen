733ba6e93e3c8a87950b39ebb5d80e9312e1c104
Alexandre <vensalex1991@gmail.com>
2023-09-26 13:48:15 +0200

    save before merging mathias

---
+++ assets/js/components/filter/category-filter/categoryFilter.js
@@ -0,0 +1,15 @@
+import { categories } from "../../../data/category.js";
+
+categories
+
+const categoryFilter = () => {
+  const divCategories = document.getElementById('list-cat');
+  
+  const btnList = document.createElement('button');
+  btnList.id = 'btn btn-list-cat';
+
+  
+
+}
+
+export { categoryFilter };
