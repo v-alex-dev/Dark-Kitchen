6503d43959cde2abdb69d1b5d779d2b9ab266dad
Barbier <mathbarbier07@gmail.com>
2023-09-27 14:57:47 +0200

    finish darkmode

--- assets/js/function/dark-mode.js
+++ assets/js/function/dark-mode.js
@@ -7,6 +7,8 @@ const darkMode = () => {
     let instagram = document.querySelector(".instagram");
     let takeaway = document.querySelector('.takeaway');
     let ubereats = document.querySelector('.ubereats');
+    let card = document.querySelector("#list-card");
+    let cat = document.querySelector("#list-cat");
 
     switchButton.addEventListener("click", () => {
         body.classList.toggle("dark-mode");
@@ -18,7 +20,8 @@ const darkMode = () => {
         instagram.srcset = isDarkMode ? "./img/social-logo/instagram-red.svg" : "./img/social-logo/instagram-black.svg";
         takeaway.srcset = isDarkMode ? "./img/social-logo/takeaway-red.svg" : "./img/social-logo/takeaway-black.svg";
         ubereats.srcset = isDarkMode ? "./img/social-logo/ubereats-red.svg" : "./img/social-logo/ubereats-black.svg";
-
+        card.style.backgroundColor = isDarkMode ? "#3C2A21" : "#E5E5CB";
+        cat.style.backgroundColor = isDarkMode ? "#1A120B":  "#E5E5CB";
     });
 };
 
