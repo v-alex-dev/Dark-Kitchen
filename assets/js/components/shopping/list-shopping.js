1d289092f14c206dffa82e74ee50ec3cf659f99e
tim <timdesmet00@gmail.com>
2023-09-27 13:59:32 +0200

    SASS shopping list

--- assets/js/components/shopping/list-shopping.js
+++ assets/js/components/shopping/list-shopping.js
@@ -1,48 +1,45 @@
 import { getDashiesByName } from "../../function/getDishesByName.js";
 
-
 // Add un element sur l'évenement click un élément li contenant le name + le prix.
 const addClickHandlers = () => {
-	const btnAdd = document.querySelectorAll("#btn-add");
+  const btnAdd = document.querySelectorAll("#btn-add");
 
-	for (const item of btnAdd) {
-		item.addEventListener("click", (e) => {
-			const listItem = document.createElement('li');
+  for (const item of btnAdd) {
+    item.addEventListener("click", (e) => {
+      const listItem = document.createElement("li");
 
-			const dishe = getDashiesByName(e.target.name);
-			const list = document.querySelector('#list-shop');
+      const dishe = getDashiesByName(e.target.name);
+      const list = document.querySelector("#list-shop");
 
-			listItem.innerHTML = `${dishe.name}  ${dishe.price.toFixed(2)}`;
-			list.appendChild(listItem);
-		});
-	}
+      listItem.innerHTML = `${dishe.name}  ${dishe.price.toFixed(2)}`;
+      list.appendChild(listItem);
+    });
+  }
 };
 
 const addClickHandlersPrice = () => {
-	const btnAdd = document.querySelectorAll("#btn-add");
-	const title = document.createElement('h5');
-	const list = document.querySelector('.shopping-total');
-	let total = 0;
-	list.appendChild(title);
+  const btnAdd = document.querySelectorAll("#btn-add");
+  const title = document.createElement("h5");
+  const list = document.querySelector(".shopping-total");
+  let total = 0;
+  list.appendChild(title);
 
-	for (const item of btnAdd) {
-		item.addEventListener("click", (e) => {
-			const dishe = getDashiesByName(e.target.name);
-			total += parseFloat(dishe.price);
-			title.innerHTML = `${total.toFixed(2)}`;
-			
-		});
-	}
+  for (const item of btnAdd) {
+    item.addEventListener("click", (e) => {
+      const dishe = getDashiesByName(e.target.name);
+      total += parseFloat(dishe.price);
+      title.innerHTML = `${total.toFixed(2)}`;
+    });
+  }
 };
 
-
 const listShop = () => {
-	const shoppingList = document.querySelector('.shopping-list');
-	const list = document.createElement('ul');
-	list.id = 'list-shop'
+  const shoppingList = document.querySelector(".shopping-list");
+  const list = document.createElement("ul");
+  list.id = "list-shop";
   shoppingList.appendChild(list);
   addClickHandlers(); // Appel de la fonction pour ajouter les gestionnaires d'événements
-	addClickHandlersPrice();
+  addClickHandlersPrice();
 };
 
 export { listShop, addClickHandlers };
